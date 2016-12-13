7c99fdff8683f7cd65835ec63ef8ffb49c77f6f5
Simon Lieb <simon@netlor.fr>
2016-12-13 13:16:35 +0100

    Indentation - php-cs-fixer

--- index.php
+++ index.php
@@ -1,51 +1,53 @@
 <?php
-if(!empty($_GET['url'])) {
-	$url		= $_GET['url'];
+if (!empty($_GET['url'])) {
+    $url = $_GET['url'];
 
-	if(strpos($url, 'http://') !== 0 && strpos($url, 'https://') !== 0) {
-		$url = 'http://'.$url;
-	}
+    if (strpos($url, 'http://') !== 0 && strpos($url, 'https://') !== 0) {
+        $url = 'http://'.$url;
+    }
 
-	$urlhash	= sha1($url);
+    $urlhash = sha1($url);
 
-	$hashfolder	= substr($urlhash, 0, 2);
-	$hashfile	= substr($urlhash, 2);
+    $hashfolder = substr($urlhash, 0, 2);
+    $hashfile = substr($urlhash, 2);
 
-	$hashfolderpath	= './db/' . $hashfolder;
-	$hashfilepath	= $hashfolderpath . '/' . $hashfile;
+    $hashfolderpath = './db/'.$hashfolder;
+    $hashfilepath = $hashfolderpath.'/'.$hashfile;
 
-	mkdir($hashfolderpath, 0700, true);
+    mkdir($hashfolderpath, 0700, true);
 
-	file_put_contents($hashfilepath, $url);
-	$content = '<a href="./?'.$urlhash.'">./?'.$urlhash.'</a><br />
+    file_put_contents($hashfilepath, $url);
+    $content = '<a href="./?'.$urlhash.'">./?'.$urlhash.'</a><br />
 			As long as there is not collision, you can shorten it down to 3 characters.';
-} else if(!empty($_GET)) {
-	$urlhash = key($_GET);
+} elseif (!empty($_GET)) {
+    $urlhash = key($_GET);
 
-	$hashfolder	= substr($urlhash, 0, 2);
-	$hashfile	= substr($urlhash, 2);
+    $hashfolder = substr($urlhash, 0, 2);
+    $hashfile = substr($urlhash, 2);
 
-	$hashfolderpath	= './db/' . $hashfolder;
-	$hashfilepath	= $hashfolderpath . '/' . $hashfile;
+    $hashfolderpath = './db/'.$hashfolder;
+    $hashfilepath = $hashfolderpath.'/'.$hashfile;
 
-	$findfiles	= glob($hashfilepath . '*');
+    $findfiles = glob($hashfilepath.'*');
 
-	if(empty($findfiles)) {
-		$content = 'No files.';
-		return 1;
-	} else if (count($findfiles) > 1) {
-		foreach($findfiles as $file) {
-			$file = str_replace('/', '', substr($file, 5));
-			$content = '<a href="./?'.$file.'">./?'.$file.'</a><br />';
-		}
-	}
+    if (empty($findfiles)) {
+        $content = 'No files.';
 
-	$fullfilepath = current($findfiles);
+        return 1;
+    } elseif (count($findfiles) > 1) {
+        foreach ($findfiles as $file) {
+            $file = str_replace('/', '', substr($file, 5));
+            $content = '<a href="./?'.$file.'">./?'.$file.'</a><br />';
+        }
+    }
 
-	header('Location:' . file_get_contents($fullfilepath));
-	return 0;
+    $fullfilepath = current($findfiles);
+
+    header('Location:'.file_get_contents($fullfilepath));
+
+    return 0;
 } else {
-	$content = '<form method="get">
+    $content = '<form method="get">
 				Enter your URL: <input type="text" name="url"><input type="submit" value="Submit">
 			</form>';
 }
